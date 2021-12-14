4a657652b3ea0fad75913220cd0ce9d22c13704c
Zhandos <zhandoslp@gmail.com>
2021-12-14 18:31:50 +0600

    Here i am did shablons of the site and i wrote the paths for html pages

--- vizitka_site/vizitka/views.py
+++ vizitka_site/vizitka/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+
 
 # Create your views here.
+
+
+
+def index(request):
+    return render(request, 'vizitka/index.html')
+
+
+def about(request):
+    return render(request, 'vizitka/about.html')
--- vizitka_site/vizitka_site/urls.py
+++ vizitka_site/vizitka_site/urls.py
@@ -14,12 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.http import HttpResponse
 
-def index(request):
-    return HttpResponse("hello index page")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('vizitka.urls'))
 ]
